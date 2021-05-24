7001f684900755043953a8402cc099ae4a23f40a
snpatel17 <35784647+snpatel17@users.noreply.github.com>
2021-05-24 16:11:02 -0400

    Initial API Setup

---
+++ DisneyCloneWebAPI/DisneyCloneWebAPI/Data/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisneyCloneWebAPI.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        { }
+        public DbSet<MovieModel> Movies { get; set; }
+
+    }
+
+}
---
+++ DisneyCloneWebAPI/DisneyCloneWebAPI/Model/MovieModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisneyCloneWebAPI.Data
+{
+    public class MovieModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MovieId { get; set; }
+
+        [Column(TypeName = "text")]
+        public string BackgroundImg { get; set; }
+
+        [Column(TypeName = "text")]
+        public string CardImg { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "text")]
+        public string SubTitle { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "text")]
+        public string TitleImg { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Type { get; set; }
+    }
+}
